5fa56933cee7e49c4b7c526d2bc8f45ec91cf486
nickglovdev <nickglovdev@gmail.com>
2020-07-29 10:17:07 -0500

    Got the dropdowns to be listed with it's key

--- src/components/questions/QuestionsList.js
+++ src/components/questions/QuestionsList.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from 'react'
-import QuestionsCard from "./QuestionsCard"
 import QuestionsManager from '../../modules/QuestionsManager'
 import QuestionCard from './QuestionsCard'
 
 const QuestionsList = (props) => {
     const [questions, setQuestion] = useState([])
+    const [number, setNumber] = useState({1:"1", 2:"2", 3:"3", 4:"4", 5:"5", 6:"6", 7:"7", 8:"8", 9:"9", 10:"10"})
 
     const getQuestion = () => {
         return QuestionsManager.getAll()
             .then(allQuestions => {
                 setQuestion(allQuestions)
             })
+    }
+    const fieldChange = evt => {
+        const stateToChange = { ...number };
+        stateToChange[evt.target.id] = evt.target.value;
+        setNumber(stateToChange);
+        console.log(stateToChange)
     }
 
     useEffect(() => {
@@ -22,6 +28,7 @@ const QuestionsList = (props) => {
             {questions.map(question => <QuestionCard
                 key={question.id}
                 question={question}
+                fieldChange={fieldChange}
                 {...props} />
             )}
         </form>
